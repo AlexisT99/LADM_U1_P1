4fe5a6547adeafa62d5bf3346c071cfdf617bef8
Alexis Torres Acosta <Torresacosta.alexis.prog@gmail.com>
2021-09-22 11:09:08 -0600

    Final

--- app/src/main/java/mx/tecnm/tepic/ladm_u1_practica1/MainActivity2.kt
+++ app/src/main/java/mx/tecnm/tepic/ladm_u1_practica1/MainActivity2.kt
@@ -2,10 +2,12 @@ package mx.tecnm.tepic.ladm_u1_practica1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mx.tecnm.tepic.ladm_u1_practica1.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        val binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
